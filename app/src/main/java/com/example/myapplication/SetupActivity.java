ca19597a8c3a3f8362111aa301e6512720f2a727
awaisraza615 <57725024+awaisraza615@users.noreply.github.com>
2019-12-04 23:47:25 +0500

    6th update registration success created setting up new accounts and login logout success

--- app/src/main/java/com/example/myapplication/SetupActivity.java
+++ app/src/main/java/com/example/myapplication/SetupActivity.java
@@ -1,10 +1,24 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,6 +27,10 @@ public class SetupActivity extends AppCompatActivity {
     private EditText UserName, FullName, CountryName;
     private Button SaveInformationbutton;
     private CircleImageView ProfileImage;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UserRef;
+    private ProgressDialog loadingBar;
+    String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +45,88 @@ public class SetupActivity extends AppCompatActivity {
         SaveInformationbutton = (Button)findViewById(R.id.setup_information_button);
         ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
 
+        loadingBar= new ProgressDialog(this);
+        mAuth= FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        UserRef = FirebaseDatabase.getInstance().getReference().child(currentUserId);
 
 
+
+        SaveInformationbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                saveAccountSetupinformation();
+            }
+
+
+        });
+
+    }
+
+    private void saveAccountSetupinformation() {
+
+        String username = UserName.getText().toString();
+        String fullname = FullName.getText().toString();
+        String country = CountryName.getText().toString();
+
+        if(TextUtils.isEmpty(username))
+        {
+            Toast.makeText(SetupActivity.this, "Please write your username...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(fullname))
+        {
+            Toast.makeText(SetupActivity.this, "Please write your full name...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(country))
+        {
+            Toast.makeText(SetupActivity.this, "Please write your country...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Saving Information");
+            loadingBar.setMessage("Please Wait, while we are your new account...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            HashMap userMap = new HashMap();
+            userMap.put("username",username);
+            userMap.put("fullname",fullname);
+            userMap.put("country",country);
+            userMap.put("status","Hey there, i am using poster social newtwork, developed by Awais Raza.");
+            userMap.put("gender","none");
+            userMap.put("dob","none");
+            userMap.put("relationshipstatus","none");
+
+            UserRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+
+                    if ( task.isSuccessful())
+                    {
+                        SendUserToMainActivity();
+                        Toast.makeText(SetupActivity.this, "your account is created succesfully", Toast.LENGTH_LONG).show();
+                        loadingBar.dismiss();
+
+                    }
+                    else
+                    {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Error occured: "+message, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void SendUserToMainActivity() {
+
+        Intent mainIntent = new Intent(SetupActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
 
     }
 }
