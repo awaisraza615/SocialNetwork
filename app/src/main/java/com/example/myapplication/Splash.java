0327c1ff5e66a08ee6782bfbe2356ecaf7184927
awaisraza615 <57725024+awaisraza615@users.noreply.github.com>
2020-02-03 00:34:12 +0500

    12th update reset password edit post save and retrieeve image success profile pic succes registration success created setting up new accounts and login logout success

---
+++ app/src/main/java/com/example/myapplication/Splash.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new  Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(Splash.this,LoginActivity.class);
+                startActivity(in);
+                finish();
+            }
+        },SPLASH_TIME);
+
+    }
+}
