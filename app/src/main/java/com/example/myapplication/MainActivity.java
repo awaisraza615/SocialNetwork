f7255ce8b25efaf45d6798e2e0f076c8be810e2c
Awais Raza <awaisraza615@gmail.com>
2020-09-05 02:27:53 +0500

    With Global News

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -405,18 +405,20 @@ public class MainActivity extends AppCompatActivity {
              case R.id.nav_profile:
                  SendUserToProfileActivity();
                  break;
-             /*case R.id.nav_home:
-                 Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show();
-                 break;*/
-            /* case R.id.nav_friends:
+             case R.id.nav_home:
+                 Intent in = new Intent(MainActivity.this,HomeActivity.class);
+                 startActivity(in);
+                 finish();
+                 break;
+             case R.id.nav_friends:
                  Toast.makeText(this,"Friend List",Toast.LENGTH_SHORT).show();
-                 break;*/
+                 break;
              case R.id.nav_find_friends:
                  SendUserToFindfriendsActivity();
                  break;
-            /* case R.id.nav_messages:
+             case R.id.nav_messages:
                  Toast.makeText(this,"Messages",Toast.LENGTH_SHORT).show();
-                 break;*/
+                 break;
              case R.id.nav_settings:
                  SendUserToSettingsActivity();
                  break;
